b86b2e92fe8519f7fbe0b288e3aedc15c3f9cfd3
jovejove <1192089188@qq.com>
2021-10-08 01:08:19 +0800

    [+] spring cloud demo

---
+++ spring_cloud/product_service/src/main/java/com/panda/product/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.panda.product.controller;
+
+import com.panda.product.entity.Product;
+import com.panda.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author: jjl
+ * @Description:
+ * @Date: 2021-10-08
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${spring.cloud.client.ip-address}") //spring cloud 自动的获取当前应用的ip地址
+    private String ip;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Product findById(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        product.setProductName("访问的服务地址:" + ip + ":" + port);
+        return product;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String save(@RequestBody Product product) {
+        productService.save(product);
+        return "保存成功";
+    }
+}
