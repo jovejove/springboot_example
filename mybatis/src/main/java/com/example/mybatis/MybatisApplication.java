56377e2c738dd3509d265391e69ba5a0bca7824f
luojunjie <1192089188@qq.com>
2021-10-15 15:11:17 +0800

    mapper接口写了@Mapper，可以不使用@MapperScan

--- mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.example.mybatis.mapper")
 public class MybatisApplication {
 
     public static void main(String[] args) {
