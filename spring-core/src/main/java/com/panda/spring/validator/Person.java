0c325974ac37e05683d5d2cac606c0d603d5dae5
luojunjie <1192089188@qq.com>
2021-10-15 16:18:14 +0800

    spring core learning

---
+++ spring-core/src/main/java/com/panda/spring/validator/Person.java
@@ -0,0 +1,12 @@
+package com.panda.spring.validator;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+    private int age;
+
+    // the usual getters and setters...
+}
