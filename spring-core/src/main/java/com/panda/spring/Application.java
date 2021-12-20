2ecb8c7549d77af98e21ba2f3f981c3021c908b9
luojunjie <1192089188@qq.com>
2021-12-20 09:37:18 +0800

    feat:删除垃圾代码

--- spring-core/src/main/java/com/panda/spring/Application.java
+++ spring-core/src/main/java/com/panda/spring/Application.java
@@ -1,13 +1,8 @@
 package com.panda.spring;
 
-import com.panda.spring.service.ClientService;
-import com.panda.spring.service.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author Administrator
--- spring-core/src/test/java/com/panda/spring/TestApplicationTest.java
+++ spring-core/src/test/java/com/panda/spring/TestApplicationTest.java
@@ -1,15 +1,8 @@
 package com.panda.spring;
 
 
-import com.panda.spring.entity.ExampleBean;
-import com.panda.spring.entity.InstantiationTracingBeanPostProcessor;
-import com.panda.spring.entity.MovieRecommender;
-import com.panda.spring.entity.Person;
-import com.panda.spring.service.ClientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class TestApplicationTest {
@@ -26,17 +19,12 @@ class TestApplicationTest {
 //    @Resource
 //    private MovieRecommender movieRecommender;
 //
-    @Resource
-    private Person person;
+
 
     @Test
     void contextLoads() {
 //        System.out.println(exampleBean.toString());
-
 //        System.out.println(clientService);
-
-        System.out.println(person);
-
 //        System.out.println(movieRecommender.getCatalog());
 
     }
