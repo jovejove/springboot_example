7bc760c2731a9a5abebe27632b40271cb4ccbfd1
luojunjie <1192089188@qq.com>
2022-01-10 10:21:38 +0800

    spring-core

---
+++ springboot-core/src/main/java/com/panda/core/SpringbootCoreApplication.java
@@ -0,0 +1,29 @@
+package com.panda.core;
+
+import com.panda.core.listener.MyApplicationStartingEventListener;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Administrator
+ */
+@EnableAsync
+@SpringBootApplication
+@ConfigurationPropertiesScan({"com.panda.core.cfg"})
+public class SpringbootCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringbootCoreApplication.class);
+        // 添加启动事件
+        application.addListeners(new MyApplicationStartingEventListener());
+        // disabled banner
+        application.setBannerMode(Banner.Mode.OFF);
+        // 禁止命令行参数加入到环境变量
+        application.setAddCommandLineProperties(false);
+        application.run(args);
+    }
+
+}
---
+++ springboot-core/src/test/java/com/panda/core/SpringbootCoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.panda.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoreApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
