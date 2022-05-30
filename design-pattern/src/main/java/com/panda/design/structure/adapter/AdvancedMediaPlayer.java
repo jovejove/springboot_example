64bec45d05efc3b5e8d6ef56eedc24e29fe3ee4b
luojunjie <1192089188@qq.com>
2022-05-30 16:33:21 +0800

    feat: 适配器模式

---
+++ design-pattern/src/main/java/com/panda/design/structure/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package com.panda.design.structure.adapter;
+
+/**
+ * 高级媒体播放器
+ */
+public interface AdvancedMediaPlayer {
+   public void playVlc(String fileName);
+   public void playMp4(String fileName);
+}
---
+++ design-pattern/src/main/java/com/panda/design/structure/adapter/MediaPlayer.java
@@ -0,0 +1,8 @@
+package com.panda.design.structure.adapter;
+
+/**
+ * 媒体播放器
+ */
+public interface MediaPlayer {
+   public void play(String audioType, String fileName);
+}
