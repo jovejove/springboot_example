7e0ab5fe55755869fec7042bb84a1a0f9f47d542
luojunjie <1192089188@qq.com>
2022-06-14 09:23:02 +0800

    feat: 冒泡排序

--- dsa/src/main/java/com/panda/sort/BubbleSort.java
+++ dsa/src/main/java/com/panda/sort/BubbleSort.java
@@ -11,10 +11,15 @@ import java.util.List;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 9, 94, 6, 2, 2, 5, 7, 8, 3, 2);
-        System.out.println(list);
-        new BubbleSort().bubbleSortV2(list);
-        System.out.println(list);
+//        List<Integer> list = Arrays.asList(1, 9, 94, 6, 2, 2, 5, 7, 8, 3, 2);
+//        System.out.println(list);
+//        new BubbleSort().bubbleSortV2(list);
+//        System.out.println(list);
+
+        int[] arr = {65461, 34, 868, 4, 5, 2, 3, 99, 5345,3, 34, 2,6564, 434};
+        System.out.println(Arrays.toString(arr));
+        new BubbleSort().bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     public void bubbleSort(List<Integer> list) {
@@ -51,5 +56,39 @@ public class BubbleSort {
         int temp = list.get(a);
         list.set(a, list.get(b));
         list.set(b, temp);
+    }
+
+
+    void swap(int[] arr,Integer a,Integer b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 是否发生交换 没有交换，提前跳出外层循环
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+    public void bubbleSortV2(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 是否发生交换 没有交换，提前跳出外层循环
+            boolean flag = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr,j,j+1);
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                return;
+            }
+        }
     }
 }
