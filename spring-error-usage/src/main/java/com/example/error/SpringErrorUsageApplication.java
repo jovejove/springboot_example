d39cc73ab763da69235a0d6e9c99258b15d23c1b
luojunjie <1192089188@qq.com>
2022-06-28 18:34:51 +0800

    feat: Spring 编程常见错误 50 例

---
+++ spring-error-usage/src/main/java/com/example/error/SpringErrorUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErrorUsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringErrorUsageApplication.class, args);
+    }
+
+}
---
+++ spring-error-usage/src/test/java/com/example/error/SpringErrorUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErrorUsageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
